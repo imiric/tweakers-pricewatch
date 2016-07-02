ba1bf4779c15b2d1de04d56429070a5b4e133c6e
Ivan Mirić <imiric@gmail.com>
2016-07-02 10:18:10 +0200

    Add CLI entrypoint

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 'use strict';
 
 module.exports = exports = require('./src');
+
+
+if (require.main === module) {
+  const argv = require('yargs').argv;
+  let ctx = {
+    secrets: {
+      MONGO_URL: process.env.MONGO_URL,
+      NOTIFY_API_TOKEN: process.env.NOTIFY_API_TOKEN
+    },
+    params: {
+      productName: argv.product,
+      notifyDeviceId: argv.device
+    }
+  };
+  exports(ctx, function(err, res) {});
+}
